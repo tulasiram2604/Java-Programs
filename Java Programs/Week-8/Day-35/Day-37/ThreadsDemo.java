f433346bccdcba0133009d642c36cfc1dd8dc897
tulasiram2604 <131224646+tulasiram2604@users.noreply.github.com>
2023-08-27 20:09:34 +0530

    Delete Java Programs/Week-8/Day-35/Day-37 directory

--- Java Programs/Week-8/Day-35/Day-37/ThreadsDemo.java
+++
@@ -1,8 +0,0 @@
-class ThreadsDemo
-  {
-    public static void main(String args[])
-    {
-      System.out.println("This is one thread"+Thread.currentThread());
-      System.out.println("This is another thread"+Thread.currentThread());
-    }
-  }
